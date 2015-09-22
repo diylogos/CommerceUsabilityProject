df7786633ed5d72836eabe3c1b3446754eda2755
SigmaMonstR <jchen85@gmail.com>
2015-09-21 23:03:30 -0400

    Updates to tiles

--- js/parallax.js
+++ js/parallax.js
@@ -5,25 +5,11 @@ function simpleParallax() {
 
     //Every element with the class "scroll" will have parallax background 
     //Change the "0.3" for adjusting scroll speed.
-    $('.scroll').css('background-position', '0' + -(scrolled * 0.3) + 'px');
+    $('.scroll').css('background-position', '0' + -(scrolled * 0.4) + 'px');
 }
 //Everytime we scroll, it will fire the function
 $(window).scroll(function (e) {
     simpleParallax();
-});
-
-//Parallax -- COVER TEXT
-function simpleParallax1a() {
-    //This variable is storing the distance scrolled
-    var scrolled = $(window).scrollTop() + 1;
-
-    //Every element with the class "scroll" will have parallax background 
-    //Change the "0.3" for adjusting scroll speed.
-    $('.scroll1a').css('text-position', '0' + -(scrolled * 0.8) + 'px');
-}
-//Everytime we scroll, it will fire the function
-$(window).scroll(function (e) {
-    simpleParallax1a();
 });
 
 //Parallax 
@@ -33,7 +19,7 @@ function simpleParallax2() {
 
     //Every element with the class "scroll" will have parallax background 
     //Change the "0.3" for adjusting scroll speed.
-    $('.scroll2').css('background-position', '0' + -(scrolled * 0.1) + 'px');
+    $('.scroll2').css('background-position', '0' + -(scrolled * 0.5) + 'px');
 }
 //Everytime we scroll, it will fire the function
 $(window).scroll(function (e) {
