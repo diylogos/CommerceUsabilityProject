c46c5fed8b7923b1e64aaf4e321a3011020432ab
SigmaMonstR <jchen85@gmail.com>
2015-10-23 16:12:23 -0400

    Updates

--- js/parallax.js
+++ js/parallax.js
@@ -5,11 +5,25 @@ function simpleParallax() {
 
     //Every element with the class "scroll" will have parallax background 
     //Change the "0.3" for adjusting scroll speed.
-    $('.scroll').css('background-position', '0' + -(scrolled * 0.4) + 'px');
+    $('.scroll').css('background-position', '0' + -(scrolled * 0.2) + 'px');
 }
 //Everytime we scroll, it will fire the function
 $(window).scroll(function (e) {
     simpleParallax();
+});
+
+//Parallax -- COVER TEXT
+function simpleParallax1() {
+    //This variable is storing the distance scrolled
+    var scrolled = $(window).scrollTop() + 1;
+
+    //Every element with the class "scroll" will have parallax background 
+    //Change the "0.3" for adjusting scroll speed.
+    $('.scroll1').css('background-position', '0' + -(scrolled * 0.2) + 'px');
+}
+//Everytime we scroll, it will fire the function
+$(window).scroll(function (e) {
+    simpleParallax1();
 });
 
 //Parallax 
@@ -19,7 +33,7 @@ function simpleParallax2() {
 
     //Every element with the class "scroll" will have parallax background 
     //Change the "0.3" for adjusting scroll speed.
-    $('.scroll2').css('background-position', '0' + -(scrolled * 0.5) + 'px');
+    $('.scroll2').css('background-position', '0' + -(scrolled * 0.2) + 'px');
 }
 //Everytime we scroll, it will fire the function
 $(window).scroll(function (e) {
